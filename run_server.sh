b7d5c1469674fee8a7eb3741d8a10593fe6a03e9
Maciej Januszewski <maciek@mjanuszewski.pl>
2016-12-11 15:42:22 +0100

    Added gunicorn

---
+++ run_server.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+clear;
+
+echo 'Deleting pyc files if there are any...'
+find . -name "*.pyc" | xargs rm
+
+echo
+
+echo 'Killing current Gunicorn instances if there are any...'
+kill -9 `ps aux | grep gunicorn | awk '{print $2}'`
+
+echo
+
+echo 'Django & OpenCV at http://localhost:8000'
+gunicorn pubmed_crawler.wsgi:application --bind 0.0.0.0:8000 --daemon
